6fb2af53834a9784dbd2e9a9d55c38a0126e3ec7
HonoreN <h.nintunze@urbilog.fr>
2017-05-05 12:29:08 +0200

    fix typo

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 'use strict';
 const corenlp = require("./index");
 
-// CoreNLP Server was lunched with french props file on port 9000
+// CoreNLP Server was started with french props file on port 9000
 corenlp.parse(
   "Bonjour le monde.", /*stringToProcess*/
   9000, /*portNumber*/
   "pos,lemma", /*annotators*/
   "json", /*outputFormat*/
   (err, parsedText) => { /*Callback function*/
-    let parsed = JSON.parse(parsedText); // we expect a JSON output
+    let parsed = JSON.parse(parsedText); // we expect a JSON output string
     console.log(JSON.stringify(parsed, null, 2));
   }
 );
